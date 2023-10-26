07c88773fe5f9418ec6124608e211bea1a393350
markhsu <xiaoyan850409@gmail.com>
2023-10-26 16:58:11 +0800

    update main.py

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -11,15 +11,6 @@ app = Flask(__name__)
 # 取得資料庫設定檔
 
 
-       try:
-            config = configparser.ConfigParser()
-            config.read("my_config.ini")
-            print("success")
-        except ConfigParser.NoOptionError as error:
-            print(error)
-        except ConfigParser.NoSectionError as error:
-            print(error)
-
 # 讀取資料庫變數
 
 #username = config.get("DB", "username")
@@ -31,35 +22,45 @@ app = Flask(__name__)
 #print("Name:", username, "PW:", password, "db:", db)
 
 # 資料庫設定
-db_settings = {
-    "host": "mysqlforpython.mysql.database.azure.com",
-    "ssl_disabled": "True",
-    "user": "markhsu",
-    "password": "Mypython_850409",
-    "db": "stock",
-    "charset": "utf8"
-}
+# db_settings = {
+    # "host": "mysqlforpython.mysql.database.azure.com",
+    # "ssl_disabled": "True",
+    # "user": "markhsu",
+    # "password": "Mypython_850409",
+    # "db": "stock",
+    # "charset": "utf8"
+# }
 
 @app.route("/")   
 def index():
-    # 建立資料庫連接
-    db = pymysql.connect(**db_settings)
+    try:
+        config = configparser.ConfigParser()
+        config.read("my_config.ini")
+        print("success")
+    except ConfigParser.NoOptionError as error:
+           print(error)
+    except ConfigParser.NoSectionError as error:
+           print(error)
+
+
+    # # 建立資料庫連接
+    # db = pymysql.connect(**db_settings)
     
-    cursor = db.cursor()
+    # cursor = db.cursor()
 
-    # 讀取資料
-    cursor.execute("SELECT * FROM stock_all_data limit 10;")
-    rows = cursor.fetchall()
-    print("Read",cursor.rowcount,"row(s) of data.")
+    # # 讀取資料
+    # cursor.execute("SELECT * FROM stock_all_data limit 10;")
+    # rows = cursor.fetchall()
+    # print("Read",cursor.rowcount,"row(s) of data.")
 
-    # 印出資料
-    for row in rows:
-      print(row)
+    # # 印出資料
+    # for row in rows:
+      # print(row)
 
-    # 釋放連線
-    db.commit()
-    cursor.close()
-    db.close()
+    # # 釋放連線
+    # db.commit()
+    # cursor.close()
+    # db.close()
 
     return "Done"
 # 啟動CGI SERVER

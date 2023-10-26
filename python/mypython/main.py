0ca716ee75830b51e7caacbbbfe66da8c7a7dae7
markhsu <xiaoyan850409@gmail.com>
2023-10-26 17:06:45 +0800

    update main.py

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -6,41 +6,27 @@ import pymysql
 import os
 app = Flask(__name__)
 
-#這邊建立MYSQL連線資訊
-
-# 取得資料庫設定檔
-
-
-# 讀取資料庫變數
-
-#username = config.get("DB", "username")
-#password = config.get("DB", "password")
-#db = config.get("DB", "db")
-#host = config.get("DB", "host")
-#ssl_disabled = config.getint("DB", "ssl_disabled") #轉換成int型態才不會出錯
-
-#print("Name:", username, "PW:", password, "db:", db)
-
-# 資料庫設定
-# db_settings = {
-    # "host": "mysqlforpython.mysql.database.azure.com",
-    # "ssl_disabled": "True",
-    # "user": "markhsu",
-    # "password": "Mypython_850409",
-    # "db": "stock",
-    # "charset": "utf8"
-# }
-
 @app.route("/")   
 def index():
     try:
+        # 取得資料庫設定檔
         config = configparser.ConfigParser()
         config.read("my_config.ini")
-        print("success")
+         # 讀取資料庫變數
+        username = config.get("DB", "username")
+        password = config.get("DB", "password")
+        db = config.get("DB", "db")
+        host = config.get("DB", "host")
+        ssl_disabled = config.getint("DB", "ssl_disabled") #轉換成int型態才不會出錯
+        print("Name:", username, "PW:", password, "db:", db)
     except ConfigParser.NoOptionError as error:
            print(error)
     except ConfigParser.NoSectionError as error:
            print(error)
+    except Exception as error:
+           print(error)
+    finally:
+           print("沒有任何錯誤，可以進行資料庫查詢了!!!")
 
 
     # # 建立資料庫連接
@@ -62,7 +48,7 @@ def index():
     # cursor.close()
     # db.close()
 
-    return "Done"
+    return "Success"
 # 啟動CGI SERVER
 if __name__ == "__main__":
   CGIHandler().run(app)

c14d502e93cb7a3932d5b7d74d8a56d4ee8a7771
markhsu <xiaoyan850409@gmail.com>
2023-10-26 09:46:41 +0800

    feat: 1.新增azure-pipelines.yml 2.新增Python部署專案

---
+++ python/mypython/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from wsgiref.handlers import CGIHandler
+from flask import Flask
+import pymysql
+import os
+app = Flask(__name__)
+
+#這邊建立MYSQL連線資訊
+
+@app.route("/")   
+def index():
+    # 建立資料庫連接
+    db = pymysql.connect(
+      host='mytestsql.mysql.database.azure.com',
+      user='mypython',
+      password='Mark850409',
+      database='school',
+      ssl_disabled='True')
+    
+    cursor = db.cursor()
+
+    # 讀取資料
+    cursor.execute("SELECT * FROM student;")
+    rows = cursor.fetchall()
+    print("Read",cursor.rowcount,"row(s) of data.")
+
+    # 印出資料
+    for row in rows:
+      print(row)
+
+    # 釋放連線
+    db.commit()
+    cursor.close()
+    db.close()
+
+    return "Done"
+# 啟動CGI SERVER
+if __name__ == "__main__":
+  CGIHandler().run(app)

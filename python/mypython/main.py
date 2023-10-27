6e709acd386999ed25c62239774f984357b4d5e8
markhsu <xiaoyan850409@gmail.com>
2023-10-27 15:29:16 +0800

    update main.py

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -4,45 +4,46 @@ from flask import Flask
 import configparser
 import pymysql
 import os
+import sys
+from MySQLdb import Error
 app = Flask(__name__)
 
 @app.route("/")   
 def index():
+    # 取得資料庫設定檔
+    config = configparser.ConfigParser()
+    config.read("my_config.ini")
+
+    # 宣告全域變數
+    global result, connection, cursor
+    # 讀取資料庫變數
+    username = config.get("DB", "username")
+    password = config.get("DB", "password")
+    db = config.get("DB", "db")
+    host = config.get("DB", "host")
+    port = config.getint("DB", "port") #轉換成int型態才不會出錯
+    # 資料庫設定
+    db_settings = {
+        "host": host,
+        "port": port,
+        "user": username,
+        "password": password,
+        "db": db,
+        "charset": "utf8"
+    }
+
     try:
-        # 取得資料庫設定檔
-        config = configparser.ConfigParser()
-        config.read("python/mypython/my_config.ini")
-    except ConfigParser.NoOptionError as error:
-           print(error)
-    except ConfigParser.NoSectionError as error:
-           print(error)
-    except Exception as error:
-           print(error)
+        # 連接 MySQL/MariaDB 資料庫
+        connection = pymysql.connect(**db_settings)  # 密碼
+        cursor = connection.cursor()
+        if connection.open:
+           print("資料庫連接成功!!!")
+    except Error as e:
+           print("資料庫連接失敗!!!", e)
     finally:
-        # 讀取資料庫變數
-        username = config['DB']['username']
-        print(username)
-        print("沒有任何錯誤，可以進行資料庫查詢了!!!")
-
-
-    # # 建立資料庫連接
-    # db = pymysql.connect(**db_settings)
-    
-    # cursor = db.cursor()
-
-    # # 讀取資料
-    # cursor.execute("SELECT * FROM stock_all_data limit 10;")
-    # rows = cursor.fetchall()
-    # print("Read",cursor.rowcount,"row(s) of data.")
-
-    # # 印出資料
-    # for row in rows:
-      # print(row)
-
-    # # 釋放連線
-    # db.commit()
-    # cursor.close()
-    # db.close()
+        if connection.open:
+           cursor.close()
+           connection.close()
 
     return "Success"
 # 啟動CGI SERVER

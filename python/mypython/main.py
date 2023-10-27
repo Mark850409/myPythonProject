0c8241224914db1d92d0ef79e562bdd5b79d4b84
markhsu <xiaoyan850409@gmail.com>
2023-10-27 16:12:02 +0800

    update main.py

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -1,56 +1,39 @@
 #!/usr/bin/python3
 from wsgiref.handlers import CGIHandler
 from flask import Flask
-import configparser
 import pymysql
 import os
-import sys
-from MySQLdb import Error
 app = Flask(__name__)
+
+#這邊建立MYSQL連線資訊
 
 @app.route("/")   
 def index():
-    # 宣告全域變數
-    global result, connection, cursor
-       
-     # 取得資料庫設定檔
-    config = configparser.ConfigParser()
-    config.read("my_config.ini")
+    # 建立資料庫連接
+    db = pymysql.connect(
+      host='mysqlforpython.mysql.database.azure.com',
+      user='markhsu',
+      password='Mypython_850409',
+      database='stock',
+      ssl_disabled='True')
+    
+    cursor = db.cursor()
 
-    try:
-        # 連接 MySQL/MariaDB 資料庫
-          # 建立資料庫連接
-        db = pymysql.connect(
-          host= 'mysqlforpython.mysql.database.azure.com',
-          user= 'markhsu',
-          password= 'Mypython_850409',
-          database= 'stock',
-          ssl_disabled='True')
-          
-        if db.open:
-           print("資料庫連接成功!!!")
-        
-        cursor = db.cursor()
+    # 讀取資料
+    cursor.execute("SELECT * FROM stock_list;")
+    rows = cursor.fetchall()
+    print("Read",cursor.rowcount,"row(s) of data.")
 
-        # 讀取資料
-        cursor.execute("SELECT * FROM stock_list;")
-        rows = cursor.fetchall()
-        print("Read",cursor.rowcount,"row(s) of data.")
+    # 印出資料
+    for row in rows:
+      print(row)
 
-        # 印出資料
-        for row in rows:
-          print(row)
-          
-        db.commit()
+    # 釋放連線
+    db.commit()
+    cursor.close()
+    db.close()
 
-    except Error as e:
-           print("資料庫連接失敗!!!", e)
-    finally:
-        if db.open:
-           cursor.close()
-           db.close()
-
-    return "Success"
+    return "Done"
 # 啟動CGI SERVER
 if __name__ == "__main__":
   CGIHandler().run(app)

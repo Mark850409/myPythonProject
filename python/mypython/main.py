a66f93ad47c59d9a24c0bb873d02aaed888ba58f
markhsu <xiaoyan850409@gmail.com>
2023-10-27 16:42:02 +0800

    update main.py

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -13,34 +13,31 @@ def index():
      # 取得資料庫設定檔
     config = configparser.ConfigParser()
     config.read("my_config.ini")
-
-    # 讀取資料庫變數
-    global username, password,db,host,ssl_disabled,cursor
+    print(config['DEFAULT']['path'])
     
+    # # 建立資料庫連接
+    # db = pymysql.connect(
+      # host= config.get("DB", "host"),
+      # user= config.get("DB", "username"),
+      # password= config.get("DB", "password"),
+      # database=config.get("DB", "db"),
+      # ssl_disabled='True')
     
-    # 建立資料庫連接
-    db = pymysql.connect(
-      host= config.get("DB", "host"),
-      user= config.get("DB", "username"),
-      password= config.get("DB", "password"),
-      database=config.get("DB", "db"),
-      ssl_disabled='True')
-    
-    cursor = db.cursor()
+    # cursor = db.cursor()
 
-    # 讀取資料
-    cursor.execute("SELECT * FROM stock_list;")
-    rows = cursor.fetchall()
-    print("Read",cursor.rowcount,"row(s) of data.")
+    # # 讀取資料
+    # cursor.execute("SELECT * FROM stock_list;")
+    # rows = cursor.fetchall()
+    # print("Read",cursor.rowcount,"row(s) of data.")
 
-    # 印出資料
-    for row in rows:
-      print(row)
+    # # 印出資料
+    # for row in rows:
+      # print(row)
 
-    # 釋放連線
-    db.commit()
-    cursor.close()
-    db.close()
+    # # 釋放連線
+    # db.commit()
+    # cursor.close()
+    # db.close()
 
     return "Done"
 # 啟動CGI SERVER

5dbd655f9ae6d71eedfc1ff0717c2a854360914a
markhsu <xiaoyan850409@gmail.com>
2023-10-29 17:51:31 +0800

    更新模板

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -6,6 +6,27 @@ import pymysql
 import os
 import sys
 import configparser
+import traceback
+import sys
+
+def abort_msg(e):
+    """500 bad request for exception
+
+    Returns:
+        500 and msg which caused problems
+    """
+    error_class = e.__class__.__name__ # 引發錯誤的 class
+    detail = e.args[0] # 得到詳細的訊息
+    cl, exc, tb = sys.exc_info() # 得到錯誤的完整資訊 Call Stack
+    lastCallStack = traceback.extract_tb(tb)[-1] # 取得最後一行的錯誤訊息
+    fileName = lastCallStack[0] # 錯誤的檔案位置名稱
+    lineNum = lastCallStack[1] # 錯誤行數 
+    funcName = lastCallStack[2] # function 名稱
+    # generate the error message
+    errMsg = "Exception raise in file: {}, line {}, in {}: [{}] {}. Please contact the member who is the person in charge of project!".format(fileName, lineNum, funcName, error_class, detail)
+    # return 500 code
+    abort(500, errMsg)
+
 app = Flask(__name__,template_folder='python/mypython/template')
 
 
@@ -37,8 +58,9 @@ def index():
 #     cursor.close()
 #     db.close()
 
-    return "HelloWorld"
+     return render_template('mypython.html')
     
 # 啟動CGI SERVER
 if __name__ == "__main__":
-  CGIHandler().run(app)
+  #CGIHandler().run(app)
+  app.run()

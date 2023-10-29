a8950e26f3656f46ef0046579cd0a3e15f07e5c2
markhsu <xiaoyan850409@gmail.com>
2023-10-29 18:52:42 +0800

    更新模板

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -8,29 +8,28 @@ import sys
 import configparser
 import traceback
 import sys
+from werkzeug.exceptions import HTTPException
+
+
+
 
 app = Flask(__name__,template_folder='python/mypython/template')
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    # pass through HTTP errors
+    if isinstance(e, HTTPException):
+        return e
+
+    # now you're handling non-HTTP exceptions only
+    return render_template("500_generic.html", e=e), 500
 
 
 
 @app.route("/")   
 def index():
-    """500 bad request for exception
 
-    Returns:
-        500 and msg which caused problems
-    """
-    error_class = e.__class__.__name__ # 引發錯誤的 class
-    detail = e.args[0] # 得到詳細的訊息
-    cl, exc, tb = sys.exc_info() # 得到錯誤的完整資訊 Call Stack
-    lastCallStack = traceback.extract_tb(tb)[-1] # 取得最後一行的錯誤訊息
-    fileName = lastCallStack[0] # 錯誤的檔案位置名稱
-    lineNum = lastCallStack[1] # 錯誤行數 
-    funcName = lastCallStack[2] # function 名稱
-    # generate the error message
-    errMsg = "Exception raise in file: {}, line {}, in {}: [{}] {}. Please contact the member who is the person in charge of project!".format(fileName, lineNum, funcName, error_class, detail)
-    # return 500 code
-    abort(500, errMsg)
 #  # 建立資料庫連接
 #     db = pymysql.connect(
 #       host='mysqlforpython.mysql.database.azure.com',

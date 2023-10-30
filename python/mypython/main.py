d7eeb5912f2c3d0c36ac257f508f438082204626
markhsu <xiaoyan850409@gmail.com>
2023-10-30 10:52:14 +0800

    fixed: 1.更新Dockerfile & main.py

--- python/mypython/main.py
+++ python/mypython/main.py
@@ -14,76 +14,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    username = config['DB']['username']
+    db = config['DB']['db']
+    host = config['DB']['host']
+    print("user:",username,"db:",db,"host:",host)
     return render_template("index.html")
 
-# def handle_error(e):
-#     code = 500
-#     error, message = str(e).split(':', 1)
+# @app.route("/")   
+# def index():
 
-#     if isinstance(e, HTTPException):
-#         code = e.code
-
-#     errors = dict(
-#         error=error,
-#         message=message.strip(),
-#         code=code,
-#         path=request.path,
-#     )
-#     return jsonify(errors=errors), code
-
-# for code in default_exceptions:
-#     app.register_error_handler(code, handle_error)
-
-# @app.errorhandler(500)
-# def error_500(exception):
-#     return jsonify({"error": str(exception)}), 500, {'Content-Type': 'application/json'}
-
-# @app.errorhandler(400)
-# def error_400(exception):
-#     return jsonify({"error": str(exception)}), 400, {'Content-Type': 'application/json'}
-
-
-# def handle_error(error):
-#     code = 500
-#     if isinstance(error, HTTPException):
-#         code = error.code
-#         print(HTTPException)
-#     return jsonify(error='error', code=code)
-
-# for exc in default_exceptions:
-#     app.register_error_handler(exc, handle_error)
-
-#@app.route("/")   
-#def index():
-
-#  # 建立資料庫連接
-#     db = pymysql.connect(
-#       host='mysqlforpython.mysql.database.azure.com',
-#       user='markhsu',
-#       password='Mypython_850409',
-#       database='stock',
-#       ssl_disabled='True')
+ # # 建立資料庫連接
+    # db = pymysql.connect(
+      # host='mysqlforpython.mysql.database.azure.com',
+      # user='markhsu',
+      # password='Mypython_850409',
+      # database='stock',
+      # ssl_disabled='True')
     
-#     cursor = db.cursor()
+    # cursor = db.cursor()
 
-#     # 讀取資料
-#     cursor.execute("SELECT * FROM stock_list limit 5;")
-#     rows = cursor.fetchall()
-#     print("Read",cursor.rowcount,"row(s) of data.")
+    # # 讀取資料
+    # cursor.execute("SELECT * FROM stock_list limit 5;")
+    # rows = cursor.fetchall()
+    # print("Read",cursor.rowcount,"row(s) of data.")
 
-#     # 印出資料
-#     for row in rows:
-#       print(row)
+    # # 印出資料
+    # for row in rows:
+      # print(row)
 
-#     # 釋放連線
-#     db.commit()
-#     cursor.close()
-#     db.close()
+    # # 釋放連線
+    # db.commit()
+    # cursor.close()
+    # db.close()
 
-   #  return render_template('mypython.html')
     
 # 啟動CGI SERVER
 if __name__ == "__main__":
   CGIHandler().run(app)
-  #app.run(debug=True)
-  #app.handle_http_exception = get_http_exception_handler(app)
